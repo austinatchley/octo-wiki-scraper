710fe90b649c7a6a8a726224fd491503a85e026e
Austin Atchley <austinatchley@gmail.com>
2024-07-21 16:34:26 -0400

    Second iteration. Fix link traversal and start to move into smaller functions

--- octo.py
+++ octo.py
@@ -1,14 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, PageElement, ResultSet
 from urllib.parse import urljoin, urlparse
 import os
 
 BASE_URL = 'https://en.wikipedia.org/wiki/'
 START_URL = 'Web_scraping'
 OUTPUT_FOLDER = 'out/' # TODO: Make this a relative path
+MAX_DEPTH = 3
 
 # Function to download HTML content of a Wikipedia page
-def download_wikipedia_html(relative_url: str, visited: set[str]):
+def download_wikipedia_html(relative_url: str, depth: int, visited: set[str]):
     if relative_url in visited:
         return
 
@@ -30,7 +31,7 @@ def download_wikipedia_html(relative_url: str, visited: set[str]):
     # print(soup.text)
     
     # Save the HTML content to a file
-    filename = relative_url
+    filename = relative_url.replace("/", "_")
     path = os.path.join(OUTPUT_FOLDER, filename)
     print(f"Path={path}")
     with open(path, 'w', encoding='utf-8') as file:
@@ -38,31 +39,45 @@ def download_wikipedia_html(relative_url: str, visited: set[str]):
     print(f"HTML content saved to '{filename}'")
     
     # Extract all links from the page
-    links = soup.find_all('a', href=True)
-    for link in links:
-        relative_link = link.get('href')
-        if relative_link.startswith('/wiki/'):
-            relative_link = relative_link.replace('/wiki/', '')
+    if depth >= MAX_DEPTH:
+        print("Reached max depth")
+        return
 
-            # Construct absolute URL
-            absolute_link = urljoin(BASE_URL, relative_link)
-            print(f"Relative link: {relative_link}")
-            print(f"Absolute link: {absolute_link}")
-            
-            # Ensure it's a Wikipedia article link and not a special page
-            if is_valid_wikipedia_link(absolute_link):
-                # Recursively download HTML of linked article
-                download_wikipedia_html(relative_link, visited)
-
-
+    for link in extract_links(soup):
+        process_link(link, depth, visited)
+    
 # Function to check if a URL is a valid Wikipedia article link
 def is_valid_wikipedia_link(url):
     parsed_url = urlparse(url)
     return parsed_url.scheme in ('http', 'https') and 'wikipedia.org' in parsed_url.netloc
+
+def extract_links(soup: BeautifulSoup) -> ResultSet[PageElement]:
+    return soup.find_all('a', href=True)
+    
+def process_link(link: PageElement, depth: int, visited: set[str]) -> None:
+    relative_link = link.get('href')
+
+    if not relative_link.startswith('/wiki/'):
+        return
+    relative_link = relative_link.replace('/wiki/', '')
+
+    # Construct absolute URL
+    absolute_link = urljoin(BASE_URL, relative_link)
+    
+    # Ensure it's a Wikipedia article link and not a special page
+    if is_valid_wikipedia_link(absolute_link):
+        print(f"Visiting linked page: {relative_link}")
+
+        # Recursively download HTML of linked article
+        download_wikipedia_html(relative_link, depth + 1, visited)
+
+
 
 if __name__ == "__main__": 
     # Create output folder if it doesn't exist
     if not os.path.exists(OUTPUT_FOLDER):
         os.makedirs(OUTPUT_FOLDER)
     
-    download_wikipedia_html(START_URL, set())
+    download_wikipedia_html(relative_url=START_URL, 
+                            depth=0, 
+                            visited=set())
